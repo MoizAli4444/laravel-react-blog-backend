af7da2d906b4c8e90cc794630660660d7949031b
Moizali4444 <moizali1280@gmail.com>
2024-05-04 14:51:11 +0500

    store blog api without image complete

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::post('blogs', [BlogController::class, 'store']);
 
 Route::get('/', function () {
     return view('welcome');
